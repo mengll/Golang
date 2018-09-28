965a8b25fb4a21678f423d91bf2b3a19d68ab839
mengll <1633094010@qq.com>
2018-09-28 09:16:53 +0800

    Create file_path.go

---
+++ libs/file_path.go
@@ -0,0 +1,23 @@
+/*
+获取程序运行路径
+*/
+func getCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println("This is an error")
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
+
+func substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+
+func getParentDirectory(dirctory string) string {
+	return substr(dirctory, 0, strings.LastIndex(dirctory, "/"))
+}
